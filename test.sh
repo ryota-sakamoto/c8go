189f1b3f3253fd9ad3f9ca44ece9a98ca8962de2
Ryota Sakamoto <sakamo.ryota+github@gmail.com>
2019-11-03 17:25:12 +0900

    fix test

--- test.sh
+++ test.sh
@@ -41,13 +41,30 @@ check 1 " 5  >= 4;"
 check 1 " 4  <= 4;"
 
 check 3 "a=3;a;"
-check 17 "a = 3; b = a + 14; b;"
+check 17 "a = 3;\
+b = a + 14;\
+b;"
 
-check 20 "foo = 1; bar = 2 + 17; foo + bar;"
-check 56 "a=1; b = a + 27; return b * 2;"
+check 20 "foo = 1;\
+bar = 2 + 17;\
+foo + bar;"
+check 56 "a=1;\
+b = a + 27;\
+return b * 2;"
 
-check 0 "if (0) return 1; return 0;"
-check 6 "a = 5; if (a == 5) a = a + 1; return a;"
-check 4 "c = 2; if (c == 2) c = 4; else return 10; return c;"
+check 0 "if (0)\
+    return 1;\
+return 0;"
+check 6 "a = 5; \
+if (a == 5) \
+    a = a + 1;\
+return a;"
+
+check 4 "c = 2;\
+if (c == 2)\
+    c = 4;\
+else\
+    return 10;\
+return c;"
 
 clean
